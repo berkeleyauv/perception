be7786b0ee2f24b0dacc69ba7cb5957dba3fff57
Marco Gellecanao <marcogelle@berkeley.edu>
2019-10-13 18:01:59 -0700

    TaskPerceiver docstrings

--- tasks/TaskPerceiver.py
+++ tasks/TaskPerceiver.py
@@ -1,17 +1,18 @@
+from typing import Any
+import numpy as np
 class TaskPerceiver:
 
     def __init__(self):
         self.time = 0
 
-    def analyze(self, frame, debug):
+    def analyze(self, frame: np.ndarray, debug: bool) -> Any:
+        """Runs the algorithm and returns the result.
+        Args:
+			frame: The frame to analyze
+            debug: Whether or not to display intermediate images for debugging
+
+		Returns:
+			the result of the algorithm
+        """
         raise NotImplementedError("Need to implement with child class.")
 
-    def calibrate(self, frame):
-        raise NotImplementedError("Need to implement with child class.")
-
-    def needs_calibration(self):
-        return False
-
-    # should be in Vis
-    #def display(self, frame, debug):
-    #    return self.analyze(frame, debug)
