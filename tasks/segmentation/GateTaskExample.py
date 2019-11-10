8d09306695358dc43ea225363f415238f0e8f3bb
Marco Gellecanao <marcogelle@berkeley.edu>
2019-11-10 15:00:38 -0800

    Averaging gate center values complete

--- tasks/segmentation/GateTaskExample.py
+++ tasks/segmentation/GateTaskExample.py
@@ -4,10 +4,12 @@ from sys import argv as args
 from combinedFilter import init_combined_filter
 import numpy as np
 import cv2 as cv
+import time
 #from segmentation.aggregateRescaling import init_aggregate_rescaling
 
 class GateTask(TaskPerceiver):
-	def analyze(self, frame: np.ndarray, debug: bool) -> Tuple[float, float]:
+	# TODO: fix the annotations on method signature about input and output type
+	def analyze(self, frame: np.ndarray, debug: bool, past_centers: [np.ndarray]) -> Tuple[float, float]:
 		"""Takes in the background removed image and returns the center between
 		the two gate posts.
 		Args:
@@ -31,17 +33,20 @@ class GateTask(TaskPerceiver):
 			box = [np.int0(b) for b in boxpts]
 			for b in box:
 				cv.drawContours(stacked_filter_frames,[b],0,(0,0,255),5)
-			#for bp in boxpts:
-			#	for corner in bp:
-			#		cv.circle(stacked_filter_frames, (corner[0], corner[1]), 10, (0,0,255), -1)
 			if len(centers) >= 2:
 				gate_center = (centers[0] + centers[1]) * 0.5
+				if len(past_centers) < 20:
+					past_centers += [gate_center]
+				else:
+					past_centers.pop(0)
+					past_centers += [gate_center]
+				gate_center = sum(past_centers) / len(past_centers)
 				gate_center = (int(gate_center[0]), int(gate_center[1]))
 				cv.circle(stacked_filter_frames, gate_center, 10, (0,255,0), -1)
 
 		if debug:
-			return ((250, 250), stacked_filter_frames)
-		return (250, 250)
+			return ((250, 250), stacked_filter_frames, past_centers)
+		return (250, 250), past_centers
 
 	def findStraightness(self, contour): # output number = contour area/convex area, the bigger the straightest
 		hull = cv.convexHull(contour, False)
@@ -53,17 +58,20 @@ class GateTask(TaskPerceiver):
 if __name__ == '__main__':
 	combined_filter = init_combined_filter()
 	cap = cv.VideoCapture(args[1])
-	ret_tries = True
+	ret_tries = 0
 	gate_task = GateTask()
 	# once = False
-	while 1 and ret_tries < 50:
+	start_time = time.time()
+	frame_count = 0
+	past_centers = []
+	while ret_tries < 50:
 		ret, frame = cap.read()
 		if ret:
 			frame = cv.resize(frame, None, fx=0.4, fy=0.4)
 
 
 			### FUNCTION CALL, can change this
-			(x, y), filtered_frame = gate_task.analyze(frame, True)
+			(x, y), filtered_frame, past_centers = gate_task.analyze(frame, True, past_centers)
 			cv.putText(frame, "x: %.2f" % x + " y: %.2f" % y,
 				(20, frame.shape[0] - 20), cv.FONT_HERSHEY_SIMPLEX,
 				2.0, (0, 165, 255), 3)
@@ -78,3 +86,5 @@ if __name__ == '__main__':
 				break
 		else:
 			ret_tries += 1
+		frame_count += 1
+		#print(frame_count / (time.time() - start_time))
