60895858055a0fb99fb2a75445815198bb7ec70a
Marco Gellecanao <marcogelle@berkeley.edu>
2019-11-10 16:56:12 -0800

    GateTaskExample.py

--- tasks/segmentation/GateTaskExample.py
+++ tasks/segmentation/GateTaskExample.py
@@ -8,8 +8,8 @@ import time
 #from segmentation.aggregateRescaling import init_aggregate_rescaling
 
 class GateTask(TaskPerceiver):
-	# TODO: fix the annotations on method signature about input and output type
-	def analyze(self, frame: np.ndarray, debug: bool, past_centers: [np.ndarray]) -> Tuple[float, float]:
+	__past_centers = []
+	def analyze(self, frame: np.ndarray, debug: bool) -> Tuple[float, float]:
 		"""Takes in the background removed image and returns the center between
 		the two gate posts.
 		Args:
@@ -29,24 +29,24 @@ class GateTask(TaskPerceiver):
 			cnts = contours[:2]
 			rects = [cv.minAreaRect(c) for c in cnts]
 			centers = [np.array(r[0]) for r in rects]
-			boxpts = [cv.boxPoints(r) for r in rects]
-			box = [np.int0(b) for b in boxpts]
-			for b in box:
-				cv.drawContours(stacked_filter_frames,[b],0,(0,0,255),5)
+			# boxpts = [cv.boxPoints(r) for r in rects]
+			# box = [np.int0(b) for b in boxpts]
+			# for b in box:
+			# 	cv.drawContours(stacked_filter_frames,[b],0,(0,0,255),5)
 			if len(centers) >= 2:
 				gate_center = (centers[0] + centers[1]) * 0.5
-				if len(past_centers) < 20:
-					past_centers += [gate_center]
+				if len(self.__past_centers) < 15:
+					self.__past_centers += [gate_center]
 				else:
-					past_centers.pop(0)
-					past_centers += [gate_center]
-				gate_center = sum(past_centers) / len(past_centers)
+					self.__past_centers.pop(0)
+					self.__past_centers += [gate_center]
+				gate_center = sum(self.__past_centers) / len(self.__past_centers)
 				gate_center = (int(gate_center[0]), int(gate_center[1]))
 				cv.circle(stacked_filter_frames, gate_center, 10, (0,255,0), -1)
 
 		if debug:
-			return ((250, 250), stacked_filter_frames, past_centers)
-		return (250, 250), past_centers
+			return ((250, 250), stacked_filter_frames)
+		return (250, 250)
 
 	def findStraightness(self, contour): # output number = contour area/convex area, the bigger the straightest
 		hull = cv.convexHull(contour, False)
@@ -63,7 +63,6 @@ if __name__ == '__main__':
 	# once = False
 	start_time = time.time()
 	frame_count = 0
-	past_centers = []
 	while ret_tries < 50:
 		ret, frame = cap.read()
 		if ret:
@@ -71,7 +70,7 @@ if __name__ == '__main__':
 
 
 			### FUNCTION CALL, can change this
-			(x, y), filtered_frame, past_centers = gate_task.analyze(frame, True, past_centers)
+			(x, y), filtered_frame = gate_task.analyze(frame, True)
 			cv.putText(frame, "x: %.2f" % x + " y: %.2f" % y,
 				(20, frame.shape[0] - 20), cv.FONT_HERSHEY_SIMPLEX,
 				2.0, (0, 165, 255), 3)
