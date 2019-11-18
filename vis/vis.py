f40fff2f140ba4819a3f6beadf7b6c01668d3518
Domas Buracas <domceb@gmail.com>
2019-11-17 17:02:47 -0800

    Update vis.py

--- vis/vis.py
+++ vis/vis.py
@@ -1,17 +1,32 @@
 import argparse
 from pathlib import Path
 
+# Collect available datasets
 data_sources = ['webcam']
-Folder = Path('../../datasets')
+datasets = Path('../../datasets')
 for file in Folder.iterdir():
     data_sources.append(file.stem)
 
+# Parse arguments
 parser = argparse.ArgumentParser(description = 'Visualizes perception algorithms.')
 parser.add_argument('--data', default = 'webcam', type=str, choices = data_sources)
 parser.add_argument('--algorithm', type=str)
 args = parser.parse_args()
 
-for file in Folder.iterdir():
-    if str(file.stem) == str(args.data):
-        for video in file.iterdir():
-            print (video)
+# Initialize image source
+if args.data == "webcam":
+    data = None 
+else:
+    data = None # datasets.joinpath(args.data).iterdir()
+
+# Main Loop
+for frame in data:
+    #TODO: benchmarking
+
+    state, debug_frames = algorithm.analyze(frame, debug=True)
+
+    cv.imshow('original', frame)
+    cv.imshow('filtered_frame', debug_frames[0]) # TODO: Yuki's visualizer here
+
+    if cv.waitKey(60) & 0xff == 27:
+        break
