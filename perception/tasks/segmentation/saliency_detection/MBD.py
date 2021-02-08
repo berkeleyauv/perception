ad9dbf648397a7ba4805e622d2cd2c4f0febd822
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-02-07 17:16:52 -0800

    Removed Internal Resizing

--- perception/tasks/segmentation/saliency_detection/MBD.py
+++ perception/tasks/segmentation/saliency_detection/MBD.py
@@ -10,10 +10,6 @@ class MBD(TaskPerceiver):
         super().__init__(lower_bound=((0,255), 10))
 
     def analyze(self, frame: np.ndarray, debug: bool, slider_vals: Dict[str, int]):
-        width = int(frame.shape[1] * 0.7)
-        height = int(frame.shape[0] * 0.7)
-        dsize = (width, height)
-        frame = cv.resize(frame, dsize)
         mbd = get_saliency_mbd(frame).astype('uint8')
         ret3,th3 = cv.threshold(mbd,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
         return th3, [frame, mbd, th3]
