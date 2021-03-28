7d0861e0ee9fc9fb0dbedf0a3ce863b71db243f6
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-03-28 16:34:48 -0700

    Added multi-contour output

--- perception/tasks/segmentation/COMB_SAL_BG.py
+++ perception/tasks/segmentation/COMB_SAL_BG.py
@@ -10,20 +10,28 @@ from shapely.geometry import Polygon
 class COMB_SAL_BG(TaskPerceiver):
     def __init__(self, **kwargs):
         super().__init__(heuristic_threshold=((5,255), 35), run_both=((0,1),0), 
-            centroid_distance_weight=((0,200),1), area_percentage_weight=((0,200),30))
+            centroid_distance_weight=((0,200),1), area_percentage_weight=((0,200),30), num_contours=((1,5), 1))
         self.sal = MBD()
         self.bg = BackgroundRemoval()
         self.use_saliency = True
         self.prev_centroid = (0,0)
         self.changed = True
         self.centroid_distance_weight = 1
-        self.area_percentage_weight = 1
+        self.area_percentage_weight = 10
+        self.num_contours = 1
 
     def filter_contours(self, contours, contour_filter):
         return filter(contour_filter, contours)
 
     def largest_contour(self, contours):
         return max(contours, key=cv.contourArea)
+
+    def largest_contours(self, contours):
+        contours.sort(key=cv.contourArea)
+        num_selected = self.num_contours
+        if len(contours) < num_selected:
+            num_selected = len(contours)
+        return contours[-self.num_contours:]
 
     def intersection_over_union(polyA, polyB):
         polygonA_shape = Polygon(polyA)
@@ -46,9 +54,10 @@ class COMB_SAL_BG(TaskPerceiver):
         analysis = algorithm(frame, debug, slider_vals=slider_vals)[0]
         _, threshold = cv.threshold(analysis, 100, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        largest_contour = self.largest_contour(contours)
+        biggest_contours = self.largest_contours(contours)
+        largest_contour = biggest_contours[-1]
         contour_frame = np.copy(frame)
-        cv.drawContours(contour_frame, [largest_contour], -1, (255,0,0))
+        cv.drawContours(contour_frame, biggest_contours, -1, (255,0,0))
         return largest_contour, [analysis, contour_frame]
 
     def analyze(self, frame: np.ndarray, debug: bool, slider_vals: Dict[str, int]):
@@ -109,6 +118,7 @@ class COMB_SAL_BG(TaskPerceiver):
         cv.drawContours(frame, [returned_contour], -1, (0,255,0))
         self.centroid_distance_weight = slider_vals['centroid_distance_weight']
         self.area_percentage_weight = slider_vals['area_percentage_weight']
+        self.num_contours = slider_vals['num_contours']
         if slider_vals['run_both']:
             return returned_contour, [frame, sal, bg, bg_frame, sal_frame]
         return returned_contour, [frame] + analysis
