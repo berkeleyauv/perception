e250126075051157f2201f3f2a8c9ee9d41af565
Michael Chigaev <mchigaev@berkeley.edu>
2023-04-08 19:34:19 -0700

    Integrated vis with symbol classifier via SymbolClassifier.py

---
+++ perception/vis/SymbolClassifier.py
@@ -0,0 +1,25 @@
+from perception.tasks.TaskPerceiver import TaskPerceiver
+from typing import Dict
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import torch
+
+class SymbolClassifier(TaskPerceiver):
+    def __init__(self, device):
+        self.model = torch.load("model.pt")
+        self.model.to(device)
+        self.model.eval()
+    def analyze(self, frame: np.ndarray, debug: bool):
+        frame_torch = torch.from_numpy(frame)
+        with torch.no_grad():
+             foundClass = self.model(frame_torch)
+        if debug:
+            cv2.putText(frame, str(foundClass), (20, 20), cv2.FONT_HERSHEY_TRIPLEX, 20, (255, 0, 0), thickness=5)
+            return foundClass, [frame]
+        return foundClass, []
+
+if __name__ == '__main__':
+    from perception.vis.vis import run
+    run(['webcam'], SymbolClassifier(), True)
+
